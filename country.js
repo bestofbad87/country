335dbea1958b0e9ac3b8222bd5ff320b25e3e8bb
root <root@ip-172-31-15-45.ap-southeast-2.compute.internal>
2023-12-08 05:20:02 +0000

    country file v.2

--- country.js
+++ country.js
@@ -11,7 +11,44 @@ async function handleRequest(request) {
   const timestamp = new Date().toISOString();
   const country = request.headers.get('cf-ipcountry');
   const countryLink = `<a href="/secure/${country.toLowerCase()}">${country}</a>`;
-  const responseBody = `${email} authenticated at ${timestamp} from ${countryLink}.`;
+  const responseBody = `
+  <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Authentication Information</title>
+        <style>
+          body {
+            font-family: 'Arial', sans-serif;
+            background-color: #f5f5f5;
+            color: #333;
+            margin: 0;
+            padding: 20px;
+            text-align: center;
+          }
+          .container {
+            max-width: 600px;
+            margin: 0 auto;
+            background-color: #fff;
+            padding: 20px;
+            border-radius: 8px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+          }
+          a {
+            color: #007bff;
+            text-decoration: none;
+          }
+        </style>
+      </head>
+      <body>
+        <div class="container">
+          <h1>Welcome Authenticated User!</h1>
+          <p>${email} authenticated at ${timestamp} from ${countryLink}.</p>
+        </div>
+      </body>
+    </html>
+  `;
 
   // This is the /secure path
   if (pathSegments.length === 1 && pathSegments[0] === 'secure') {
